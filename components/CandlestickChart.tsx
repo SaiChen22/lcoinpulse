03b89b0585f18a89911ab118130128e63fb19288
Sai_Chen <saikaungsanchen@gmail.com>
2026-01-15 21:53:44 -0500

    implement candlestick chart

---
+++ components/CandlestickChart.tsx
@@ -0,0 +1,127 @@
+'use client'
+import { getCandlestickConfig, getChartConfig, PERIOD_BUTTONS, PERIOD_CONFIG } from '@/constants'
+import { CandlestickSeries, IChartApi, ISeriesApi } from 'lightweight-charts'
+import { fetcher } from '@/lib/coingecko.actions'
+import React, { useEffect } from 'react'
+import { start } from 'repl'
+import { create } from 'domain'
+
+import { createChart } from 'lightweight-charts'
+import { convertOHLCData } from '@/lib/utils'
+
+const CandlestickChart = ({ children, data, coinId, height = 360, initialPeriod = 'daily' }: CandlestickChartProps) => {
+	const chartContainerRef = React.useRef<HTMLDivElement | null>(null)
+	const chartRef = React.useRef<IChartApi>(null)
+	const candleSeriesRef = React.useRef<ISeriesApi<'Candlestick'>>(null)
+
+	const [loading, setLoading] = React.useState(false)
+	const [period, setPeriod] = React.useState<Period>(initialPeriod)
+	const [ohlcData, setOhlcData] = React.useState<OHLCData[]>(data ?? [])
+	const [isPending, startTransition] = React.useTransition()
+
+	const fetchOhlcData = async (selectedPeriod: Period) => {
+		try {
+			const { days, interval } = PERIOD_CONFIG[selectedPeriod];
+
+			const newData = await fetcher<OHLCData[]>(`/coins/${coinId}/ohlc`, {
+				vs_currency: 'usd',
+				days,
+				precision: 'full',
+			})
+
+			setOhlcData(newData ?? []);
+		}
+		catch (error) {
+			console.error('Error fetching OHLC data:', error);
+		}
+
+
+	}
+
+	const handlePeriodChange = (newPeriod: Period) => {
+		if (newPeriod === period) return;
+
+		startTransition(async () => {
+			setPeriod(newPeriod);
+			await fetchOhlcData(newPeriod);
+
+		})
+	}
+
+	useEffect(() => {
+		const container = chartContainerRef.current;
+		if (!container) return;
+
+		const showTime = ['daily', 'weekly', 'monthly'].includes(period);
+		const chart = createChart(container, {
+			...getChartConfig(height, showTime),
+			width: container.clientWidth,
+		});
+
+		const series = chart.addSeries(CandlestickSeries, getCandlestickConfig())
+		series.setData(convertOHLCData(ohlcData));
+		chart.timeScale().fitContent();
+
+		chartRef.current = chart;
+		candleSeriesRef.current = series;
+
+		const observer = new ResizeObserver((entries) => {
+			if (entries.length === 0) return;
+			chart.applyOptions({ width: entries[0].contentRect.width })
+		});
+
+		observer.observe(container);
+
+		return () => {
+			observer.unobserve(container);
+			observer.disconnect();
+			chart.remove();
+
+			chartRef.current = null;
+			candleSeriesRef.current = null;
+		}
+
+	}, [height])
+
+	useEffect(() => {
+		if (!candleSeriesRef.current) return;
+
+		const convertedToSeconds = ohlcData.map(
+			(item) => [Math.floor(item[0] / 1000), item[1], item[2], item[3], item[4]] as OHLCData
+		)
+
+		const converted = convertOHLCData(convertedToSeconds)
+		candleSeriesRef.current.setData(converted);
+		chartRef.current?.timeScale().fitContent();
+
+	}, [ohlcData, period])
+
+
+	return (
+		<div id="candlestick-chart">
+			<div className='chart-header'>
+				<div className='flex-1'>
+					{children}
+				</div>
+
+				<div className='button-group'>
+					<span className='text-sm mx-2 font-medium text-purple-100/50'>
+						Period:
+					</span>
+					{
+						PERIOD_BUTTONS.map(({ value, label }) => (
+							<button key={value} className={period === value ? 'config-button-active' : 'config-button'}
+								onClick={() => handlePeriodChange(value)} disabled={loading}>{label}</button>
+
+						))
+					}
+				</div>
+			</div>
+			<div ref={chartContainerRef} className='chart' style={{ height }} />
+
+		</div>
+
+	)
+}
+
+export default CandlestickChart
--- components/home/CoinOverView.tsx
+++ components/home/CoinOverView.tsx
@@ -2,24 +2,43 @@ import React from 'react'
 import Image from 'next/image'
 import { formatCurrency } from '@/lib/utils'
 import { fetcher } from '@/lib/coingecko.actions'
+import { CoinOverviewFallback } from './fallback'
+import CandlestickChart from '../CandlestickChart'
 
 const CoinOverView = async () => {
 
-    const coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
-        dex_pair_format: 'symbol'
-    })
+    try {
+        const [coin, coinOHLCData] = await Promise.all([
+            await fetcher<CoinDetailsData>('/coins/bitcoin', {
+                dex_pair_format: 'symbol'
+            }),
+            await fetcher<OHLCData[]>('/coins/bitcoin/ohlc', {
+                vs_currency: 'usd',
+                days: 1,
+                precision: 'full'
+            })
+        ])
 
-    return (
-        <div id="coin-overview">
-            <div className='header pt-2'>
-                <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-                <div className="info">
-                    <p>{coin.name} / {coin.symbol.toUpperCase()}</p>
-                    <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
-                </div>
+        return (
+
+            <div id="coin-overview">
+                <CandlestickChart data={coinOHLCData} coinId="bitcoin">
+                    <div className='header pt-2'>
+                        <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
+                        <div className="info">
+                            <p>{coin.name} / {coin.symbol.toUpperCase()}</p>
+                            <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+                        </div>
+                    </div>
+                </CandlestickChart>
             </div>
-        </div>
-    )
+        )
+    }
+    catch (error) {
+        console.error('Error fetching coin overview', error);
+        return <CoinOverviewFallback />
+    }
+
 }
 
 export default CoinOverView
